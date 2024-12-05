2d329e834de70c57c7dcfb7b0b737ba6d75e53b6
Joshua Escio <joscio21@gmail.com>
2024-12-05 22:57:56 +0800

    added auto-tagging system in details

--- thesis_design_database_manager/models/article_publication.py
+++ thesis_design_database_manager/models/article_publication.py
@@ -1,5 +1,5 @@
 from odoo import api,fields,models
-from difflib import SequenceMatcher
+from difflib import SequenceMatcher, get_close_matches
 
 class ArticlePublication(models.Model):
     _name = "article.publication"
@@ -56,13 +56,35 @@ class ArticlePublication(models.Model):
                 similar_tags = set(article.article_tag_ids.ids) & set(self.article_tag_ids.ids)
                 article.related_score = len(similar_tags)
                 seq_match = SequenceMatcher(None, self.name,article.name)
-                article.similarity_score = seq_match.ratio()
+                article.similarity_score = (seq_match.ratio())*100
                 if article.related_score > article.max_related_score:
                     article.max_related_score = article.related_score
                 if article.similarity_score > self.similarity_score:
                     self.max_similarity_score = article.similarity_score
         else:
             self.related_article_ids = [(5, 0, 0)]
+
+    def act_suggested_tags(self):
+        if self.name:
+            all_tags = self.env['article.tag'].search([])
+            tag_names = [tag.name for tag in all_tags] #get all tags to compare
+            
+            keywords = self.name.split()
+            for word in keywords:
+                found_tags = get_close_matches(word, tag_names) #compare all words in sentence to existing tags (up to 3 letters mistake)
+            
+            suggested_tags = self.env['article.tag'].search([
+                ('name', 'in', found_tags),
+                ("id","not in", self.article_tag_ids.ids) #all tags in found tags and not currently assigned to this model
+                ])
+            existing_tags = self.env['article.tag'].search([
+                ("id","in",self.article_tag_ids.ids) #all tags currently assigned to this model
+            ])
+            
+            combined_tags = (suggested_tags)|(existing_tags)
+            self.article_tag_ids = [(6,0, combined_tags.ids)]
+        
+        return
 
     def act_view_article(self):
         self.ensure_one()
@@ -74,3 +96,6 @@ class ArticlePublication(models.Model):
             'res_id': self.id,
             'target': 'current',
         }
+    
+
+
